f99bb11eac5eee2dea0df5ae747bf2756565759b
Nezirić Aldin <neziric.aldin@nsoft.com>
2018-10-11 10:46:26 +0200

    Add component tests

---
+++ src/components/Details.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h4>Vue unit tests Requirements</h4>
+    <code>
+      npm i -D @vue/test-utils @vue/server-test-utils vue-server-renderer babel-jest
+    </code>
+    <p :class="['message', { 'iAmEmpty': newMsg === ''}]">{{ newMsg }}</p>
+    <h3></h3>
+    <div>
+      <button
+        class="okay"
+        @click="()=>{ newMsg = 'You are so OK! 😎' }">Okay!</button>
+    </div>
+    <br>
+    <div>
+      <button
+        class="remove"
+        @click="()=>{ newMsg = '' }">Clear!</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Details',
+  data() {
+    return {
+      newMsg: '',
+    };
+  },
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+</style>
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,18 +1,106 @@
-import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import { renderToString } from '@vue/server-test-utils';
+import Details from '@/components/Details.vue';
 import App from '@/App.vue';
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
+const localVue = createLocalVue();
+
+describe('🍄 App component:', () => {
+  it('App is a Vue instance', () => {
+    const wrapper = shallowMount(App, {
+      localVue,
+    });
+    expect(wrapper.isVueInstance()).toBeTruthy();
+  });
+
+  it('App sets data', () => {
+    const wrapper = shallowMount(App, {
+      localVue,
+      mocks: { count: 0 }
+    });
+    expect(wrapper.vm.count).toBe(0);
+    wrapper.setData({ count: 10 });
+    expect(wrapper.vm.count).toBe(10);
+  });
+
+  it('App renders an image', () => {
+    const wrapper = renderToString(App);
+    expect(wrapper).toContain('<img');
+  });
+
+});
+
+describe('👾 Details component:', () => {
+  it('Details renders props.msg when passed', () => {
     const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(Details, {
+      localVue,
       propsData: { msg },
     });
     expect(wrapper.text()).toMatch(msg);
   });
 
-  it('App is a Vue instance', () => {
-    const wrapper = shallowMount(App);
-    expect(wrapper.isVueInstance()).toBeTruthy();
+  it('Button sets message data', () => {
+    const wrapper = shallowMount(Details, {
+      localVue,
+    });
+    const btn = wrapper.find('button');
+    btn.trigger('click');
+    expect(wrapper.vm.newMsg).toMatch('You are so OK!');
   });
+
+  it('Button removes message data', () => {
+    const wrapper = shallowMount(Details, {
+      localVue,
+    });
+    const btn = wrapper.find('button.remove');
+    btn.trigger('click');
+    expect(wrapper.vm.newMsg).toMatch('');
+  });
+
+  it('Buttons toggle classes', () => {
+    const wrapper = shallowMount(Details);
+    const btn = wrapper.find('button.okay');
+    btn.trigger('click');
+    expect(wrapper.find('.message').classes()).not.toContain('iAmEmpty');
+    const btn2 = wrapper.find('button.remove');
+    btn2.trigger('click');
+    expect(wrapper.find('.message').classes()).toContain('iAmEmpty');
+  });
+
+  it('Details contains h3 tags',  () => {
+    const wrapper = shallowMount(Details);
+    expect(wrapper.findAll('h3').exists()).toBe(true);
+  });
+
+  it('H3 tag is empty', () => {
+    const wrapper = shallowMount(Details);
+    expect(wrapper.find('h3').isEmpty()).toBe(true);
+  });
+
+  it('Sets props on update', () => {
+    const msg = 'Default message';
+    const wrapper = shallowMount(Details, {
+      localVue,
+      propsData: { msg },
+    });
+    expect(wrapper.text()).toMatch(msg);
+
+    // Setting new props value as update from parent component
+    const newMsg = 'Updated message';
+    wrapper.setProps({ msg: newMsg });
+    expect(wrapper.text()).toMatch(newMsg);
+  });
+
+  it('Calls a method once when destroyed', () => {
+    const onDestroyMethod = jest.fn();
+    const wrapper = shallowMount(Details, {
+      destroyed () {
+        onDestroyMethod();
+      }
+    }).destroy();
+
+    expect(onDestroyMethod.mock.calls.length).toBe(1);
+  });
+
 });
